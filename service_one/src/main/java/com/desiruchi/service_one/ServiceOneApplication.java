24ffc1e073261a64a8afcf4471b0fbf8c187268f
madepu <madepu@nmdp.org>
2018-04-16 14:37:14 -0500

    adding service_one spring boot project

---
+++ service_one/src/main/java/com/desiruchi/service_one/ServiceOneApplication.java
@@ -0,0 +1,12 @@
+package com.desiruchi.service_one;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceOneApplication.class, args);
+	}
+}
