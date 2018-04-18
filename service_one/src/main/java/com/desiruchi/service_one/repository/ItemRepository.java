01578bae5bfeae9cad5b8e316483d185e91868dd
madepu <madepu@nmdp.org>
2018-04-18 16:39:03 -0500

    Spring Data RestositoryRestREsource added Please enter the commit message for your changes. Lines starting

--- service_one/src/main/java/com/desiruchi/service_one/repository/ItemRepository.java
+++ service_one/src/main/java/com/desiruchi/service_one/repository/ItemRepository.java
@@ -1,8 +1,17 @@
 package com.desiruchi.service_one.repository;
 
+
 import com.desiruchi.service_one.model.Item;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+/**
+ * Desc: POST: Create
+         PUT: Total update
+         PATCH: Partial update
+         DELETE: Delete
+         GET: View
+ */
+@RepositoryRestResource(collectionResourceRel = "items", path = "items")
 public interface ItemRepository extends MongoRepository<Item,String>{
-    public Item findByIname(String iname);
 }
