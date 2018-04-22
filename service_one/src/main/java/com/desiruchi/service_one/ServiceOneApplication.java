b1c5a9b53371394cbaccb10009d64472e3764de4
madepu <madepu@nmdp.org>
2018-04-22 18:07:08 -0500

    commit

--- service_one/src/main/java/com/desiruchi/service_one/ServiceOneApplication.java
+++ service_one/src/main/java/com/desiruchi/service_one/ServiceOneApplication.java
@@ -1,5 +1,7 @@
 package com.desiruchi.service_one;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServiceOneApplication {
 
 	public static void main(String[] args) {
+
+		final Logger logger = LogManager.getLogger(ServiceOneApplication.class);
+		logger.debug("Debugging log");
+		logger.info("Info log");
+		logger.warn("Hey, This is a warning!");
+		logger.error("Oops! We have an Error. OK");
+		logger.fatal("Damn! Fatal error. Please fix me.");
+
 		SpringApplication.run(ServiceOneApplication.class, args);
 	}
 }
