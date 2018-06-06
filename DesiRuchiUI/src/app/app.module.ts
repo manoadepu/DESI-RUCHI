44693f46a54089081ba82a156e1b12a735887e2f
madepu <madepu@nmdp.org>
2018-06-05 21:59:21 -0500

    app module update

--- DesiRuchiUI/src/app/app.module.ts
+++ DesiRuchiUI/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrowserModule } from '@angular/platform-browser';
