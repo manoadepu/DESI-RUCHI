7c57f37eb5c93411541ad1837a66f76f5a41495f
madepu <madepu@nmdp.org>
2018-06-06 16:17:56 -0500

    added rest call in food

--- DesiRuchiUI/src/app/food/food.component.ts
+++ DesiRuchiUI/src/app/food/food.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-food',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FoodComponent implements OnInit {
 
-  constructor() { }
+  title = 'app';
+  itemsFromMongo: items;
+  constructor(private http: HttpClient) {
+    }
 
   ngOnInit() {
+
+    this.getItems().subscribe(response => {
+      this.itemsFromMongo = response._embedded.items;
+      console.log('items:::', this.itemsFromMongo);
+    });
   }
 
+  getItems(): Observable<any> {
+    return this.http.get('http://localhost:8080/items')
+    .map(
+      response => {
+        return response;
+        // JSON.parse(response.json());
+        // console.log(response.json()._embedded.items);
+      }
+    );
+
+  }
+
+}
+
+interface items {
+  ingredients: string[];
+  itemCategory: string;
+  itemName: string;
+  nutritionFacts: string[];
+  price: number;
+  rating: string;
 }
