aa11d5d4b2f058f1088262eaba19f0c7d192768f
dev-mannem <swathichowdary.mannem@gmail.com>
2018-09-23 10:53:05 -0400

    fixing authentication

--- DesiRuchiUI/src/app/sellerlogin/sellerlogin.component.ts
+++ DesiRuchiUI/src/app/sellerlogin/sellerlogin.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { SellerLoginServiceService } from '../services/seller-login-service.service';
 import { SellerLoginModel } from '../models/seller-login-model';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FoodSellerModel } from '../models/food-seller-model';
 @Component({
   selector: 'app-sellerlogin',
   templateUrl: './sellerlogin.component.html',
@@ -15,6 +16,8 @@ export class SellerloginComponent implements OnInit {
   sellerModel: SellerLoginModel;
   sellerForm: FormGroup;
   routerLink: String;
+  foodSeller: FoodSellerModel;
+    result: Boolean = false;
   constructor(private httpClient: HttpClient, private fb: FormBuilder) {
     console.log('in seller login component');
     this.sellerForm = this.fb.group({
@@ -35,7 +38,16 @@ export class SellerloginComponent implements OnInit {
     console.log(this.sellerModel);
     console.log('before calling method in service...');
     console.log('routerLink: ' + this.routerLink);
-    this.routerLink = this.sellerLoginServiceService.validateCredentialsAndSendRouterLink(this.sellerModel);
-    console.log('{{routerLink}} ' + this.routerLink);
+    this.sellerLoginServiceService.validateCredentialsAndSendRouterLink(this.sellerModel).subscribe(response => {
+      this.foodSeller = response._embedded.FoodSellers[0];
+      if (this.sellerModel.email === this.foodSeller.email && this.sellerModel.password === this.foodSeller.password) {
+        this.result = true;
+        this.routerLink = 'seller';
+        console.log('router link in validateCredentialsAndSendRouterLink::: Success ' + this.routerLink);
+      } else {
+        this.routerLink = 'food/stores';
+        console.log('router link in validateCredentialsAndSendRouterLink::: Failure ' + this.routerLink);
+    }
+    });
   }
 }
--- DesiRuchiUI/src/app/services/seller-login-service.service.ts
+++ DesiRuchiUI/src/app/services/seller-login-service.service.ts
@@ -3,39 +3,26 @@ import { HttpClient } from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import { map } from 'rxjs/operators';
 import { SellerLoginModel } from '../models/seller-login-model';
-import { FoodSellerModel } from '../models/food-seller-model';
 @Injectable()
 export class SellerLoginServiceService {
   routerLink: String = 'wrong';
-  result: Boolean = false;
-  foodSeller: FoodSellerModel;
   constructor(private httpClient: HttpClient) {console.log('in sellerlogin service'); }
 
   /**
    *
    * @param sellerLoginModel
    */
-  sellerLoginValidation(sellerLoginModel: SellerLoginModel) {
+  sellerLoginValidation(sellerLoginModel: SellerLoginModel): Observable<any> {
     console.log('in sellerLoginvalidation');
-    this.getFoodSeller(sellerLoginModel).subscribe(response => {
-      this.foodSeller = response._embedded.FoodSellers[0];
-      if (sellerLoginModel.email === this.foodSeller.email && sellerLoginModel.password === this.foodSeller.password) {
-        this.result = true;
-        this.routerLink = 'seller';
-      } else {
-        this.routerLink = 'food/stores';
-    }
-    });
+    return this.getFoodSeller(sellerLoginModel);
   }
 
   /**
  *
  * @param sellerLoginModel
  */
-validateCredentialsAndSendRouterLink(sellerLoginModel: SellerLoginModel): String {
-  this.sellerLoginValidation(sellerLoginModel);
-  console.log('router link in validateCredentialsAndSendRouterLink: ' + this.routerLink);
-  return this.routerLink;
+validateCredentialsAndSendRouterLink(sellerLoginModel: SellerLoginModel) {
+  return this.sellerLoginValidation(sellerLoginModel);
 }
 
 /**
